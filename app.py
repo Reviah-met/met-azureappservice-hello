72053adf06aa250fe3ef5e89d5b564b9592bb8a5
Reviah-met <50105052@belfastmet.ac.uk>
2024-10-17 15:59:53 +0100

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'You've been unhacked!'
+    return 'You've been hacked!'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
